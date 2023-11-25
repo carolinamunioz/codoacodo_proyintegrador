079c048ccaa03a5421ef1e57de3fe49b709a804a
Carolina Muñoz <carolinamunoz@MBP-de-Carolina.fibertel.com.ar>
2023-11-25 16:37:04 -0300

    Mission 5: primeros pasos

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use('/admin',adminRoutes);
 
 /* Motor de Plantillas EJS */
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'./src/views'));
+app.set('views', './src/views');
 
-app.listen(4000, () => console.log("Servidor running on http://localhost:4000"));
+app.listen(4000, () => console.log("Server running on http://localhost:4000"));
