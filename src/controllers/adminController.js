80559ffdd9cb0d91cfc2a9e1b1b411aed8bbeea0
Carolina Muñoz <carolinamunoz@MacBook-Pro-de-Carolina.local>
2023-12-10 23:48:19 -0300

    [FEAT] Adding express session for Admin pages

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -73,3 +73,160 @@ export const deleteProduct = async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 }
+
+
+
+//A partir de aca, session validator
+
+// LOGIN
+export const loginPOST = async (req, res) => {
+
+    console.log("ENVIANDO INFO A LOGIN")
+    /* const password = req.body.password
+    const usuario = req.body.usuario */
+    const { usuario, password } = req.body
+
+    const userData = await getUserByUsernameFromDB(usuario)
+    // userData ---> { usuario: "Admin", password: "12345"}
+
+    // si obtuvo algo de la base de datos
+    if (userData && userData.password == password) {
+      
+        req.session.esAdmin = true
+        req.session.usuario = usuario
+        console.log("REQ SESSION ---> ",req.session)
+        res.redirect("/admin")
+    } else {
+        res.render('login', {
+            mensaje: "Nombre de usuario o contraseña incorrecta",
+            error: true
+        });
+    }
+}
+
+// READ 
+export const getAllUsuarios = async (req, res) => {
+    try {
+        const datos = await getAllUsuariosFromDB() 
+        res.render("adminUsuarios", {
+            usuario: req.session.usuario,
+            data: datos,
+            mensaje: req.query.mensaje || ""
+        })
+    } catch (error) {
+        console.error('Error getting usuarios:', error);
+        res.status(500).send('Internal Server Error');
+    }
+} 
+
+// Agregar usuario - VISTA
+export const addUsuario = async (req, res) => {
+    try {
+        res.render("adminCrear", {
+            titulo: "Crear usuario",
+            huboError: false,
+            errores: []
+        })
+    } catch (error) {
+        console.error('Error adding usuario:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+// Agregar usuario - POST (CREATE)
+export const addUsuarioPOST = async (req, res) => {
+
+    console.log("req.file -->", req.file) // Obtener los datos del ARCHIVO subido
+    console.log("req.body -->", req.body) // Obtener los datos de TEXTO del formulario
+
+    const newUsuarioData = req.body;
+    newUsuarioData.imagen = req.file.filename
+
+    try {
+
+        // Validación
+        const errores = validationResult(req)
+        console.log("ERRORES -->", errores)
+
+        if (!errores.isEmpty()) {
+            return res.render("adminCrear", {
+                titulo: "Crear Usuario - Hubo errores",
+                huboError: true,
+                errores: errores.errors, // mensajes de error en array
+            })
+
+        }
+
+        await addUsuarioFromDB(newUsuarioData);
+        res.redirect("/admin/login" + "?mensaje=Usuario agregado")
+    } catch (error) {
+        console.error('Error adding Usuario:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+// Editar usuario - VISTA
+export const editUsuario = async (req, res) => {
+    const usuarioID = req.params.id;
+
+    try {
+        const usuario = await getUsuarioPorIDFromDB(usuarioID);
+        if (usuario) {
+            // res.status(200).json(usuario);
+            res.render("adminActualizar", {
+                data: usuario
+            })
+        } else {
+            res.status(404).send('Usuario not found');
+        }
+    } catch (error) {
+        console.error('Error getting usuario by ID:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+// Editar usuario - POST (UPDATE)
+export const editUsuarioPOST = async (req, res) => {
+    const usuarioID = req.params.id;
+    const updatedUsuarioData = req.body;
+    updatedUsuarioData.imagen = req.file.filename;
+
+    try {
+
+        // Poner chequeo validacion
+
+        const updatedUsuario = await editUsuarioPostFromDB(usuarioID, updatedUsuarioData);
+        if (updatedUsuario) {
+            // res.status(200).json(updatedUsuario);
+            res.redirect("/admin" + "?mensaje=Usuario actualizado")
+        } else {
+            res.status(404).send('Usuario not found');
+        }
+    } catch (error) {
+        console.error('Error editing Usuario:', error);
+        res.status(500).send('Internal Server Error')
+    }
+}
+
+export const deleteUsuario = async (req, res) => {
+    const usuarioID = req.params.id;
+    try {
+        
+        const deletedUsuario = await deleteUsuarioFromDB(usuarioID);
+
+        if (deletedUsuario) {
+
+            unlink('public/uploads/' + deletedUsuario.imagen, (err) => {
+                if (err) res.send(`Ocurrió un error ${err.code}`);
+                console.log('Usuario borrado');
+            }); 
+            // res.status(200).json(deletedUsuario);
+            res.redirect("/admin" + "?mensaje=Usuario Borrado")
+        } else {
+            res.status(404).send('usuario not found');
+        }
+    } catch (error) {
+        console.error('Error deleting usuario:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,11 +2,20 @@ import { Router } from 'express';
 const router = Router();
 import { getAllProducts, createItem, saveNewProduct, getProductById, updateProduct, deleteProduct} from '../controllers/adminController.js';
 
-router.get('/', getAllProducts);
-router.get('/create', createItem);
-router.post('/create', saveNewProduct); 
-router.get('/edit/:id', getProductById);
-router.post('/edit/:id', updateProduct);
-router.get('/delete/:id', deleteProduct);
+// session
+const requiereAdmin = (req, res, next) => {
+    if (!req.session.esAdmin) {
+      return res.redirect("/auth/login");
+    }
+    next();
+};
+
+
+router.get('/', requiereAdmin, getAllProducts);
+router.get('/create',requiereAdmin, createItem);
+router.post('/create',requiereAdmin, saveNewProduct); 
+router.get('/edit/:id',requiereAdmin, getProductById);
+router.post('/edit/:id',requiereAdmin, updateProduct);
+router.get('/delete/:id',requiereAdmin, deleteProduct);
 
 export default router;
